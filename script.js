4f76af247178cef871937b6026a3247b04725c15
claudiamoreno <60939893+claudiamoreno@users.noreply.github.com>
2020-05-13 18:09:49 +0200

    menuburguer

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*HOME SCROLL BUTTON*/
+
 $('.js-anchor').on('click', function(event) {
     event.preventDefault();
     var target = $(this).attr('href');
@@ -10,10 +12,27 @@ $('.js-anchor').on('click', function(event) {
     );
 });
 
+/*MENU BUTTON*/
+
 $ ('.js-open').on('click', function(){
-    $('html').addClass('menu-open');
+
+    if($('html').class('menu-open')){
+      $('html').addClass('menu-open');
+    } else {
+      $('html').removeClass('menu-open')
+    }
+
 });
 
 $ ('.js-close').on('click', function(){
     $('html').removeClass('menu-open');
+});
+
+
+
+$(document).ready(function() {
+  $(".hamburguer-bt").click(function() {
+    $(this).toggleClass("on");
+    $('html').toggleClass('menu-open');
+  });
 });
